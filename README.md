4ea87354f188d379d7a2d644ece0b9b7e4c9db0f
LoreleiAurora <iam@l5i.me>
2017-07-02 03:13:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# VVV Custom site template (develop)
+For when you just need a simple dev site
+
+## Overview
+This template will allow you to create a WordPress dev environment  runninr wordpress src using only `vvv-custom.yml`.
+
+The supported environments are:
+- A single site
+- A subdomain multisite
+- A subdirectory multisite
+
+# Configuration
+
+### The minimum required configuration:
+
+```
+my-site:
+  repo: https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+  hosts:
+    - my-site.dev
+```
+| Setting    | Value       |
+|------------|-------------|
+| Domain     | my-site.dev |
+| Site Title | my-site.dev |
+| DB Name    | my-site     |
+| Site Type  | Single      |
+
+### WordPress Multisite with Subdomains:
+
+```
+my-site:
+  repo: https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+  hosts:
+    - multisite.dev
+    - site1.multisite.dev
+    - site2.multisite.dev
+  custom:
+    wp_type: subdomain
+```
+| Setting    | Value               |
+|------------|---------------------|
+| Domain     | multisite.dev       |
+| Site Title | multisite.dev       |
+| DB Name    | my-site             |
+| Site Type  | Subdomain Multisite |
+
+## Configuration Options
+
+```
+hosts:
+    - foo.dev
+    - bar.dev
+    - baz.dev
+```
+Defines the domains and hosts for VVV to listen on. 
+The first domain in this list is your sites primary domain.
+
+```
+custom:
+    site_title: My Awesome Dev Site
+```
+Defines the site title to be set upon installing WordPress.
+
+```
+custom:
+    wp_type: single
+```
+Defines the type of install you are creating.
+Valid values are:
+- single
+- subdomain
+- subdirectory
+
+```
+custom:
+    db_name: super_secet_db_name
+```
+Defines the DB name for the installation.
+
+
---
+++ provision/vvv-init.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".dev`
+DOMAINS=`get_hosts "${DOMAIN}"`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+WP_TYPE=`get_config_value 'wp_type' "single"`
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+
+# Make a database, if we don't already have one
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\n DB operations done.\n\n"
+
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+# Install and configure the latest stable version of WordPress
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/src/wp-load.php" ]]; then
+  echo "Checking out WordPress trunk. See https://develop.svn.wordpress.org/trunk"
+  noroot svn checkout "https://develop.svn.wordpress.org/trunk/" "${VVV_PATH_TO_SITE}/public_html"
+  cd "${VVV_PATH_TO_SITE}/public_html"
+  noroot npm install
+else
+  cd "${VVV_PATH_TO_SITE}/public_html"
+  echo "Updating WordPress trunk. See https://develop.svn.wordpress.org/trunk"
+  svn up
+  noroot npm install &>/dev/null
+  grunt
+fi
+
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/src/wp-config.php" ]]; then
+  cd "${VVV_PATH_TO_SITE}/public_html"
+  echo "Configuring WordPress trunk..."
+  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+define( 'WP_DEBUG', true );
+PHP
+fi
+
+if ! $(noroot wp core is-installed); then
+  cd ${VVV_PATH_TO_SITE}
+  echo "Installing WordPress trunk..."
+
+  if [ "${WP_TYPE}" = "subdomain" ]; then
+    INSTALL_COMMAND="multisite-install --subdomains"
+  elif [ "${WP_TYPE}" = "subdirectory" ]; then
+    INSTALL_COMMAND="multisite-install"
+  else
+    INSTALL_COMMAND="install"
+  fi
+
+  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+fi
+
+if [[ ! -d "${VVV_PATH_TO_SITE}/public_html/build" ]]; then
+  echo "Initializing grunt... This may take a few moments."
+  cd "${VVV_PATH_TO_SITE}/public_html/"
+  grunt
+fi
+
+mkdir -p "${VVV_PATH_TO_SITE}/public_html/src/wp-content/mu-plugins" "${VVV_PATH_TO_SITE}/public_html/build/wp-content/mu-plugins"
+ln -sf "${VVV_PATH_TO_SITE}/provision/vvv.php" "${VVV_PATH_TO_SITE}/public_html/src/wp-content/mu-plugins/vvv.php"
+ln -sf "${VVV_PATH_TO_SITE}/provision/vvv.php" "${VVV_PATH_TO_SITE}/public_html/build/wp-content/mu-plugins/vvv.php"
+
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
