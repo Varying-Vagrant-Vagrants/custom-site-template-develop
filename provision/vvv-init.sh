3a394e83dcd3486ed555309db64766faed4a7794
Tom J Nowell <contact@tomjn.com>
2019-12-04 15:18:56 +0000

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -4,53 +4,53 @@ set -eo pipefail
 
 echo "Custom Site Template Develop Provisioner - use this template for WP Core development. For client work, use the custom-site-template instead"
 
-DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
-SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
-WP_TYPE=`get_config_value 'wp_type' "single"`
-DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DOMAIN=$(get_primary_host "${VVV_SITE_NAME}".test)
+SITE_TITLE=$(get_config_value 'site_title' "${DOMAIN}")
+WP_TYPE=$(get_config_value 'wp_type' "single")
+DB_NAME=$(get_config_value 'db_name' "${VVV_SITE_NAME}")
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+echo -e " * Creating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-echo -e "\nGranting the wp user priviledges to the '${DB_NAME}' database"
+echo -e " * Granting the wp user priviledges to the '${DB_NAME}' database"
 mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
-echo -e "\n DB operations done.\n\n"
+echo -e " * DB operations done."
 
-echo "Setting up the log subfolder for Nginx logs"
-noroot mkdir -p ${VVV_PATH_TO_SITE}/log
-noroot touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
-noroot touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
+echo " * Setting up the log subfolder for Nginx logs"
+noroot mkdir -p "${VVV_PATH_TO_SITE}/log"
+noroot touch "${VVV_PATH_TO_SITE}/log/nginx-error.log"
+noroot touch "${VVV_PATH_TO_SITE}/log/nginx-access.log"
 
 echo "Creating public_html folder if it doesn't exist already"
-noroot mkdir -p ${VVV_PATH_TO_SITE}/public_html
+noroot mkdir -p "${VVV_PATH_TO_SITE}/public_html"
 
-date_time=`cat /vagrant/provisioned_at`
+date_time=$(cat /vagrant/provisioned_at)
 logfolder="/var/log/provisioners/${date_time}"
 logfile="${logfolder}/provisioner-${VVV_SITE_NAME}-grunt.log"
 
 # Install and configure the latest stable version of WordPress
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/src/wp-load.php" ]]; then
-  echo "Checking out WordPress trunk. See https://develop.svn.wordpress.org/trunk"
+  echo " * Checking out WordPress trunk. See https://develop.svn.wordpress.org/trunk"
   noroot svn checkout "https://develop.svn.wordpress.org/trunk/" "${VVV_PATH_TO_SITE}/public_html"
   cd "${VVV_PATH_TO_SITE}/public_html"
-  echo "Running npm install"
+  echo " * Running npm install"
   noroot npm install --no-optional
 else
   cd "${VVV_PATH_TO_SITE}/public_html"
-  echo "Updating WordPress trunk. See https://develop.svn.wordpress.org/trunk"
+  echo " * Updating WordPress trunk. See https://develop.svn.wordpress.org/trunk"
   if [[ -e .svn ]]; then
-    echo "Running svn up"
+    echo " * Running svn up"
     noroot svn up
   else
     if [[ $(noroot git rev-parse --abbrev-ref HEAD) == 'master' ]]; then
       echo "running git pull --no-edit git://develop.git.wordpress.org/ master"
       noroot git pull --no-edit git://develop.git.wordpress.org/ master
     else
-      echo "Skipped auto git pull on develop.git.wordpress.org since you aren't on the master branch"
+      echo " * Skipped auto git pull on develop.git.wordpress.org since you aren't on the master branch"
     fi
   fi
-  echo "Running npm install"
+  echo " * Running npm install"
   # Grunt can crash because doesn't find a folder, the workaround is remove the node_modules folder and download all the dependencies again.
   # We create a file with the stderr output of NPM to check if there are errors, if yes we remove the folder and try again npm install.
   noroot npm install --no-optional &> /tmp/dev-npm.txt
@@ -58,18 +58,18 @@ else
     rm -rf node_modules
     noroot npm install --no-optional
   fi
-  echo "Running grunt"
-  echo " Check the Grunt/Webpack output for Trunk at VVV/log/provisioners/${date_time}/provisioner-${VVV_SITE_NAME}-grunt.log"
-  noroot grunt > $logfile 2>&1 
+  echo " * Running grunt"
+  echo " * Check the Grunt/Webpack output for Trunk at VVV/log/provisioners/${date_time}/provisioner-${VVV_SITE_NAME}-grunt.log"
+  noroot grunt > "${logfile}" 2>&1 
   if [ $? -ne 0 ]; then
-     echo "Grunt exited with an error, last 10 lines of log:"
-     tail -10 $logfile
+     echo " ! Grunt exited with an error, last 10 lines of log:"
+     tail -10 "${logfile}"
   fi
 fi
 
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
   cd "${VVV_PATH_TO_SITE}/public_html"
-  echo "Configuring WordPress trunk..."
+  echo " * Configuring WordPress trunk..."
   noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --path="${VVV_PATH_TO_SITE}/public_html/src" --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'SCRIPT_DEBUG', true );
@@ -79,8 +79,8 @@ PHP
 fi
 
 if ! $(noroot wp core is-installed --path="${VVV_PATH_TO_SITE}/public_html/src"); then
-  cd ${VVV_PATH_TO_SITE}
-  echo "Installing WordPress trunk..."
+  cd "${VVV_PATH_TO_SITE}"
+  echo " * Installing WordPress trunk..."
 
   if [ "${WP_TYPE}" = "subdomain" ]; then
     INSTALL_COMMAND="multisite-install --subdomains"
@@ -90,11 +90,11 @@ if ! $(noroot wp core is-installed --path="${VVV_PATH_TO_SITE}/public_html/src")
     INSTALL_COMMAND="install"
   fi
 
-  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password" --path="${VVV_PATH_TO_SITE}/public_html/src"
-  echo "WordPress Source was installed at ${VVV_PATH_TO_SITE}/public_html/src, with the username 'admin', and the password 'password'"
+  noroot wp core "${INSTALL_COMMAND}" --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password" --path="${VVV_PATH_TO_SITE}/public_html/src"
+  echo " * WordPress Source was installed at ${VVV_PATH_TO_SITE}/public_html/src, with the username 'admin', and the password 'password'"
 fi
 
-echo "Setting up the WP importer"
+echo " * Setting up the WP importer"
 if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
   cd "${VVV_PATH_TO_SITE}/public_html/tests/phpunit/data/plugins/"
   if [[ -e 'wordpress-importer/.svn' ]]; then
@@ -103,27 +103,27 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
   else
     noroot svn checkout https://plugins.svn.wordpress.org/wordpress-importer/tags/0.6.3/ wordpress-importer
   fi
-  cd ${VVV_PATH_TO_SITE}
+  cd "${VVV_PATH_TO_SITE}"
 fi
 
-echo "Checking for WordPress build"
+echo " * Checking for WordPress build"
 if [[ ! -d "${VVV_PATH_TO_SITE}/public_html/build" ]]; then
-  echo "Initializing grunt... This may take a few moments."
+  echo " * Initializing grunt... This may take a few moments."
   cd "${VVV_PATH_TO_SITE}/public_html/"
-  echo " Check the Grunt/Webpack output for Trunk Build at VVV/log/provisioners/${date_time}/provisioner-${NAME}-grunt.log"
-  noroot grunt > $logfile 2>&1 
-  echo "Grunt initialized."
+  echo " * Check the Grunt/Webpack output for Trunk Build at VVV/log/provisioners/${date_time}/provisioner-${NAME}-grunt.log"
+  noroot grunt > "${logfile}" 2>&1 
+  echo " * Grunt initialized."
 fi
-echo "Checking mu-plugins folder"
+echo " * Checking mu-plugins folder"
 noroot mkdir -p "${VVV_PATH_TO_SITE}/public_html/src/wp-content/mu-plugins" "${VVV_PATH_TO_SITE}/public_html/build/wp-content/mu-plugins"
 
-echo "Copying the sites Nginx config template"
+echo " * Copying the sites Nginx config template"
 if [ -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" ]; then
-  echo "A vvv-nginx-custom.conf file was found"
+  echo " * A vvv-nginx-custom.conf file was found"
   cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 else
-  echo "Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
+  echo " * Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
   cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-default.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 fi
 
-echo "Custom site template develop provisioner completed, WP will be served from the build folder"
+echo " * Custom site template develop provisioner completed, WP will be served from the build folder"
