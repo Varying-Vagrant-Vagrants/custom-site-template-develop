fc31ef362e942c01532c458bae15f47e19d85bcd
Daniele Scasciafratte <mte90net@gmail.com>
2020-07-06 15:29:59 +0200

    typo

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -10,7 +10,7 @@ DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 VCS=$(get_config_value 'vcs' '')
 
 if [[ -f "${VVV_PATH_TO_SITE}/public_html/.svn" && $VCS == "git" ]] || [[ "${VVV_PATH_TO_SITE}/public_html/.git" && $VCS == "svn" ]]; then
-  echo " * VCS doesn't match an existing repo, probably you set a different VCS compraed to the one used by this template"
+  echo " * VCS doesn't match an existing repo, probably you set a different VCS compared to the one used by this template"
 fi
 
 if [[ -z "${VCS}" ]]; then
